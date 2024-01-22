363a0b8ade68e5f80aca20a6b372196b33541956
MaksGorobets <gorobets542@gmail.com>
2024-01-22 20:15:46 +0200

    Day 75 update

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -11,6 +11,20 @@ struct ContentView: View {
     
     @State public var countiesFlags = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Spain", "Ukraine", "UK", "US"].shuffled()
     
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes. Top stripe blue, middle stripe black, bottom stripe white.",
+        "France": "Flag with three vertical stripes. Left stripe blue, middle stripe white, right stripe red.",
+        "Germany": "Flag with three horizontal stripes. Top stripe black, middle stripe red, bottom stripe gold.",
+        "Ireland": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe orange.",
+        "Italy": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe red.",
+        "Nigeria": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe green.",
+        "Poland": "Flag with two horizontal stripes. Top stripe white, bottom stripe red.",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red.",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background.",
+        "Ukraine": "Flag with two horizontal stripes. Top stripe blue, bottom stripe yellow.",
+        "US": "Flag with many red and white stripes, with white stars on a blue background in the top-left corner."
+    ]
+    
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var alertIsPresented = false
     @State private var finalAlertIsPresented = false
@@ -50,6 +64,7 @@ struct ContentView: View {
                         } label: {
                             FlagView(country: countiesFlags[number])
                         }
+                        .accessibilityLabel(labels[countiesFlags[number], default: "Unknown"])
                         .rotation3DEffect(
                             .degrees(selectedFlag[number] ? 360 : 0), axis: (x: 0.0, y: 1.0, z: 0.0)
                         )
